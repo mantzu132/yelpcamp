d14184157349170581ce4408a328f2eab7b8d159
Defixenas <defixass@gmail.com>
2023-09-10 20:51:54 +0300

    cluster map + home page

--- index.js
+++ index.js
@@ -1,118 +1,111 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const engine = require('ejs-mate');
-const path = require('path');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user');
-
-
+const engine = require("ejs-mate");
+const path = require("path");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 //----------------------------------------------------------------------
 
 //----------------------------------------------------------------------------
 
 // For storing sessions
-const session = require('express-session')
+const session = require("express-session");
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
-app.use(session(sessionConfig))
-
+  secret: "thisshouldbeabettersecret!",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 
 //Passport authentication
 app.use(passport.initialize());
 app.use(passport.session()); // Makes passport owrk with express-session^
-passport.use(new LocalStrategy(User.authenticate())) // Use the local passport strategy and use this function to authenticate.
+passport.use(new LocalStrategy(User.authenticate())); // Use the local passport strategy and use this function to authenticate.
 //Telling passport how to serialize/deserialize a user object (User.serializeUser() function) ... deserializeUser())
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 //flashing messages
-const flash = require('connect-flash');
+const flash = require("connect-flash");
 app.use(flash());
 
 //Add the flash messages / current user object to the locals object (so views can access)
 app.use((req, res, next) => {
-    res.locals.messages = req.flash()
-    res.locals.loggedIn = req.isAuthenticated();
-    if (req.user) {
-        res.locals.userId = req.user._id;
-    }
-    next();
+  res.locals.messages = req.flash();
+  res.locals.loggedIn = req.isAuthenticated();
+  if (req.user) {
+    res.locals.userId = req.user._id;
+  }
+  next();
 });
-
 
 // Mongoose model
 // const Review = require('./models/reviews');
 // const Campground = require('./models/campground')
 // const ExpressError = require('./utils/ExpressError');
 
-
 // DATABASE CONNECTION --------------------------------------------------------------------
-const dbName = 'yelp-camp'; // Name your database here
-const dbUrl = 'mongodb://localhost:27017/' + dbName;
+const dbName = "yelp-camp"; // Name your database here
+const dbUrl = "mongodb://localhost:27017/" + dbName;
 
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('Database connection open');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Database connection open");
 });
 
 // DATABASE CONNECTION END  --------------------------------------------------------------------
 
 // Templating and templating engine.
 
-app.engine('ejs', engine);
-app.set('view engine', 'ejs')
-
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
 
 // View folder absolute directory path for consistency
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
 // So we get HTML status code with requests in console.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // So we can use PUT and DELETE in forms.
 app.use(express.urlencoded({ extended: true }));
 
 // So we can edit our campgrounds as HTML forms don't support PUT or DELETE.
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 // serve static files in public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routers
-const campgroundRoutes = require('./routes/campgrounds');
-app.use('/campgrounds', campgroundRoutes)
+const campgroundRoutes = require("./routes/campgrounds");
+app.use("/campgrounds", campgroundRoutes);
 
-const reviewRoutes = require('./routes/reviews');
-app.use('/campgrounds/:id/reviews', reviewRoutes)
+const reviewRoutes = require("./routes/reviews");
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-const usersRoutes = require('./routes/users');
-app.use('/users', usersRoutes)
-
+const usersRoutes = require("./routes/users");
+app.use("/users", usersRoutes);
 
 // ROUTES START HERE---------------------------------------------------
 
+app.get("/", async (req, res, next) => {
+  res.render("campgrounds/home");
+});
 
 // ROUTES END HERE ---------------------------------------------------
-
 
 // When we can't find a page pass error to the error handler
 // app.use((req, res, next) => {
@@ -121,12 +114,11 @@ app.use('/users', usersRoutes)
 
 // ERROR HANDLER
 app.use((err, req, res, next) => {
-    if (!err.message) err.message = 'Oh no! Something went wrong!';
-    const { statusCode = 500 } = err;
-    res.status(statusCode).render('error', { err });
+  if (!err.message) err.message = "Oh no! Something went wrong!";
+  const { statusCode = 500 } = err;
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
-    console.log(`Success! Your application is running on port 3000.`);
+  console.log(`Success! Your application is running on port 3000.`);
 });
-
--- seeds/index.js
+++ seeds/index.js
@@ -1,57 +1,61 @@
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelper');
-const Campground = require('../models/campground');
-const Review = require('../models/reviews');
-const User = require('../models/user');
-
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelper");
+const Campground = require("../models/campground");
+const Review = require("../models/reviews");
+const User = require("../models/user");
 
 // Delete everything in DB and add in random camp objects
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
-
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    await Review.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            images: [
-                {
-                    url: 'https://res.cloudinary.com/debmdj6wp/image/upload/v1693852716/YelpCamp/rpk5rn6wwnjpxu8edaxq.jpg',
-                    fileName: 'YelpCamp/rpk5rn6wwnjpxu8edaxq'
-                },
-                {
-                    url: 'https://res.cloudinary.com/debmdj6wp/image/upload/v1693852716/YelpCamp/okjhmydpliaqkir8xrlz.jpg',
-                    fileName: 'YelpCamp/okjhmydpliaqkir8xrlz'
-                }
-            ],
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis veniam animi recusandae possimus minus provident sequi mollitia vel laborum perspiciatis excepturi explicabo, pariatur quis porro veritatis omnis dolor, impedit dignissimos.',
-            price,
+  await Campground.deleteMany({});
+  await Review.deleteMany({});
+  for (let i = 0; i < 300; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      images: [
+        {
+          url: "https://res.cloudinary.com/debmdj6wp/image/upload/v1693852716/YelpCamp/rpk5rn6wwnjpxu8edaxq.jpg",
+          fileName: "YelpCamp/rpk5rn6wwnjpxu8edaxq",
+        },
+        {
+          url: "https://res.cloudinary.com/debmdj6wp/image/upload/v1693852716/YelpCamp/okjhmydpliaqkir8xrlz.jpg",
+          fileName: "YelpCamp/okjhmydpliaqkir8xrlz",
+        },
+      ],
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis veniam animi recusandae possimus minus provident sequi mollitia vel laborum perspiciatis excepturi explicabo, pariatur quis porro veritatis omnis dolor, impedit dignissimos.",
+      price,
 
-            author: '64ece77f1e1073e2e1437c00',
-            geometry: { type: 'Point', coordinates: [ 25.2829111, 54.6870458 ] }
-
-        })
-        await camp.save();
-    }
-}
+      author: "64ece77f1e1073e2e1437c00",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
-})
+  mongoose.connection.close();
+});
